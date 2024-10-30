81b718c58827441a19ad0388c30f27e44f9c7108
sirine kahweji <sirinekahweji5@gmail.com>
2024-10-30 21:04:54 +0100

    navbar accueilPage

--- Frontend/src/app/accueil/accueil.component.ts
+++ Frontend/src/app/accueil/accueil.component.ts
@@ -1,12 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AddpostComponent } from './addpost/addpost.component';
+
+
 
 
 @Component({
   selector: 'app-accueil',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [CommonModule,AddpostComponent ,RouterOutlet, RouterLink, RouterLinkActive,NavbarComponent],
   templateUrl: './accueil.component.html',
   styleUrl: './accueil.component.css'
 })
--- Frontend/src/app/home/home.component.ts
+++ Frontend/src/app/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { NavbarComponent } from '../navbar/navbar.component'
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive,NavbarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
