457843ef42e0b570a8eca22668f7e461cc1b8d1b
Akram Zaabi <akramzaabi7@gmail.com>
2024-11-16 00:42:28 +0100

    fixed front_colors and event_micro_service

--- Backend/service_event2/demo/src/main/java/com/example/demo/repositories/ParticipationRepository.java
+++ Backend/service_event2/demo/src/main/java/com/example/demo/repositories/ParticipationRepository.java
@@ -4,6 +4,7 @@ package com.example.demo.repositories;
 import com.example.demo.entities.Participation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ParticipationRepository extends JpaRepository<Participation,Long> {
-    Participation findByPersonIdAndEventId(Long personId, Long eventId);
+public interface ParticipationRepository extends JpaRepository<Participation, Long> {
+
 }
+
