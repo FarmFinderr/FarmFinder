64cd73ba021d6fffd90c6507a275301355fffd99
iheb <ihebsessi29@gmail.com>
2024-11-16 11:52:36 +0100

    addpostpage

---
+++ Backend/service_notifications/Notification-service/src/test/java/com/example/Notification_service/TestcontainersConfiguration.java
@@ -0,0 +1,8 @@
+package com.example.Notification_service;
+
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+}
