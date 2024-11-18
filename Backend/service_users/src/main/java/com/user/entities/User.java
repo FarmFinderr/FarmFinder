b6acc5c331005edf4b7dacf265f4804d1bd89b5d
wessim <wessimsaidani56@gmail.com>
2024-11-18 00:06:23 +0000

    user service back

---
+++ Backend/service_users/src/main/java/com/user/entities/User.java
@@ -0,0 +1,100 @@
+package com.user.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nom;
+    private String prenom;
+    private int age;
+    private boolean sexe;
+    private String addresse;
+    private String email;
+    private String photo;
+    private int role;
+	public User() {
+		super();
+	}
+	public User(Long id, String nom, String prenom, int age, boolean sexe, String addresse, String email, String photo,
+			int role) {
+		super();
+		this.id = id;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.age = age;
+		this.sexe = sexe;
+		this.addresse = addresse;
+		this.email = email;
+		this.photo = photo;
+		this.role = role;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public boolean isSexe() {
+		return sexe;
+	}
+	public void setSexe(boolean sexe) {
+		this.sexe = sexe;
+	}
+	public String getAddresse() {
+		return addresse;
+	}
+	public void setAddresse(String addresse) {
+		this.addresse = addresse;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhoto() {
+		return photo;
+	}
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+	public int getRole() {
+		return role;
+	}
+	public void setRole(int role) {
+		this.role = role;
+	}
+    
+}
---
+++ Backend/user_srvice/src/test/java/com/example/user_srvice/UserSrviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.user_srvice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserSrviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
