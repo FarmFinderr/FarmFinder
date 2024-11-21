2da47e0ca41f6cfc6be85ef56e7b211520b39e1d
sirine kahweji <sirinekahweji5@gmail.com>
2024-11-21 19:29:14 +0100

    first commit

--- projet_integration/app/build.gradle.kts
+++ projet_integration/app/build.gradle.kts
@@ -42,13 +42,13 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
-    implementation ("androidx.appcompat:appcompat:1.4.0")
-    implementation ("com.google.android.material:material:1.4.0")
+    implementation(libs.androidx.recyclerview)
+    implementation (libs.androidx.appcompat.v140)
+    implementation (libs.material.v140)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation("com.github.bumptech.glide:glide:4.15.1") // Assurez-vous que cette version est à jour
+    implementation(libs.glide) // Assurez-vous que cette version est à jour
 
 
 }
