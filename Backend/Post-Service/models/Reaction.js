473f9ce4008a8bd1b4569e5daa23a552b6bac536
sirine kahweji <sirinekahweji5@gmail.com>
2024-11-22 18:29:31 +0100

    les tables image video reaction

---
+++ Backend/Post-Service/models/Reaction.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const reactionSchema = new mongoose.Schema({
+    postId: { type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: true },
+    userId: { type: String, required: true },
+    reactionType: { 
+        type: String, 
+        enum: ['like', 'love', 'haha', 'wow', 'sad', 'angry'], 
+        required: true 
+    },
+    date: { type: Date, default: Date.now }
+});
+
+const Reaction = mongoose.model('Reaction', reactionSchema);
+
+export default Reaction;
