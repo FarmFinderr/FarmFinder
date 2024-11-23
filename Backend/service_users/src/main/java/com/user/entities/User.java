fe3070a9b71ff7e820b883fcd29f54b091c732c0
Akram Zaabi <akramzaabi7@gmail.com>
2024-11-23 19:29:22 +0100

    changes

--- Backend/service_users/src/main/java/com/user/entities/User.java
+++ Backend/service_users/src/main/java/com/user/entities/User.java
@@ -11,8 +11,6 @@ import lombok.ToString;
 
 @Entity
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 @ToString
 public class User {
     @Id
--- projet_integration/app/src/main/java/com/example/projet_integration/SignupActivity.kt
+++ projet_integration/app/src/main/java/com/example/projet_integration/SignupActivity.kt
@@ -1,6 +1,8 @@
 package com.example.projet_integration
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,7 +12,19 @@ class SignupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_signup2)
+        setContentView(R.layout.activity_signup)
+
+        val spinner: Spinner = findViewById(R.id.gendre_spinner)
+        val adapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.gender_options,
+            android.R.layout.simple_spinner_item
+        )
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
