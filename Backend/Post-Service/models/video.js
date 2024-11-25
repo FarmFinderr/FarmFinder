36cfd31ba0e71c59ae2187c6586caf8123be6b74
sirine kahweji <sirinekahweji5@gmail.com>
2024-11-25 16:24:32 +0100

    resolve confli

--- Backend/Post-Service/models/video.js
+++ Backend/Post-Service/models/video.js
@@ -3,10 +3,8 @@ import mongoose from 'mongoose';
 const videoSchema = new mongoose.Schema({
     postId: { type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: true },
     path: { type: String, required: true }, 
-    date: { 
-        type: Date, 
-        default: Date.now 
-    }});
+    date: { type: Date, default: Date.now }
+});
 
 const Video = mongoose.model('Video', videoSchema);
 
