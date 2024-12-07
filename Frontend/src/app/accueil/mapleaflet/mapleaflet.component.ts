3c4df1651141e2c766a2864fda0e9ec520040080
sirine kahweji <sirinekahweji5@gmail.com>
2024-12-07 09:29:20 +0100

    reglage acceil resolution et insatller un test de map

---
+++ Frontend/src/app/accueil/mapleaflet/mapleaflet.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mapleaflet',
+  standalone: true,
+  imports: [],
+  templateUrl: './mapleaflet.component.html',
+  styleUrl: './mapleaflet.component.css'
+})
+export class MapleafletComponent {
+
+}
