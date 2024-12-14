4609c925241995bcd45d14faef278e9be551d049
sirine kahweji <sirinekahweji5@gmail.com>
2024-12-15 00:33:43 +0100

    partie sign in front

--- Frontend/src/app/sign-in/sign-in.component.ts
+++ Frontend/src/app/sign-in/sign-in.component.ts
@@ -1,13 +1,45 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { UserService } from '../services/user/user.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent,CommonModule, FormsModule],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.css'
 })
 export class SignInComponent {
+
+  username: string = '';
+  password: string = '';
+
+  constructor(private userService: UserService, private router: Router) {}
+
+  onSubmit(): void {
+    if (this.username && this.password) {
+      console.log("email",this.username);
+      console.log("password",this.password);
+      this.userService.login(this.username, this.password).subscribe({
+        next: (response) => {
+
+          localStorage.setItem('token', response.token);
+          console.log('Connexion réussie', response);
+
+          // Redirigez l'utilisateur vers une page protégée
+          this.router.navigate(['/accueil']);
+        },
+        error: (err) => {
+          console.error('Erreur de connexion', err);
+          alert('Email ou mot de passe incorrect');
+        },
+      });
+    } else {
+      alert('Veuillez remplir tous les champs');
+    }
+  }
 
 }
--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -5,8 +5,12 @@ module.exports = {
       "./src/**/*.{html,ts}",
     ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        playfair: ['"Playfair Display"', 'serif'],
+    },
   },
   plugins: [require('tailwind-scrollbar-hide')],
+}
 }
 
